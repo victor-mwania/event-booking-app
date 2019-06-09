225bb049f39b95267f0b6afec9d0eb44606d8dae
victor-mwania <victormwnza@gmail.com>
2019-06-09 20:41:01 +0300

    added nav gaurd

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,9 +3,10 @@ import Router from 'vue-router'
 import Login from './views/Login.vue'
 import SignUp from './views/SignUp.vue'
 import Tickets from './views/Tickets.vue'
+import CreateEvents from './views/CreateEvents.vue';
 Vue.use(Router)
 
-export default new Router({
+let router =  new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -27,7 +28,44 @@ export default new Router({
     {
       path: '/tickets',
       name: 'tickets',
-      component: Tickets
+      component: Tickets,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/create-events',
+      name: 'create-events',
+      component: CreateEvents,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)){
+    if(localStorage.getItem('jwt') == null){
+      next({
+        path: '/login',
+        params: {nextUrl: to.fullPath}
+      })
+    }
+    else {
+      next()
+    }
+  }
+  else if(to.matched.some(record => record.meta.guest)) {
+    if(localStorage.getItem('jwt') == null){
+        next()
+    }
+    else{
+        next({ name: 'home'})
+    }
+}else {
+    next() 
+}
+})
+
+export default router;
