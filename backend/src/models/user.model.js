bd7acaf8f80786dbe8d0b3eb8cb581fa6ccfc006
victor-mwania <victormwnza@gmail.com>
2019-06-09 20:47:12 +0300

    added events

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    userName = String,
+    userName: String,
     password: String,
+    events: [String]
 })
 
 const user = mongoose.model('usermodel', userSchema);
 
-module.export = user;
+module.exports = user;
