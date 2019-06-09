ac6ae40b59c8f481b9762d755fb7d8ab80409e02
victor-mwania <victormwnza@gmail.com>
2019-06-09 20:46:19 +0300

    added db url

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const events = require('./routes/events.route');
+const user = require('./routes/user.route');
+
+mongoose.connect('mongodb://victor:victor1234@cluster0-shard-00-00-vfoph.mongodb.net:27017,cluster0-shard-00-01-vfoph.mongodb.net:27017,cluster0-shard-00-02-vfoph.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true' || 'mongodb://localhost:27017/sync', { useNewUrlParser: true });
 
 const app = express();
 app.use(morgan('dev'));
@@ -11,9 +14,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors({optionsSuccessStatus: 200}));
 
-mongoose.connect(process.env.MONGOLAB_CRIMSON_URI ||'mongodb://localhost:27017/pandora', { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(user);
 app.use(events);
-app.listen(3000, ()=> {
-    console.log("Listening on port "+ 3000);
+const port = process.env.PORT || 4400;
+app.listen(port, ()=> {
+    console.log("Listening on port "+ port);
 })
